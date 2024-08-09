d156258cb700c297615798f47cc6aed55868c837
Miguel <menjivarmiguel221@gmail.com>
2024-08-08 21:24:11 -0700

    another push

--- firebase-react-app/src/App.js
+++ firebase-react-app/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Write from './components/Write';
+import Read from './components/Read';
+import UpdateRead from "./components/UpdateRead";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
         <Routes>
           <Route path="/" element={ <Write />} />
           <Route path="/write" element={ <Write />} />
+          <Route path="/read" element={ <Read />} />
+          <Route path="/updateread" element={ <UpdateRead />} />
         </Routes>
       </Router>
     </div>
